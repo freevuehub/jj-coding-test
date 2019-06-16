aaeb347204c1daec0e65a97323cbb5faf1fc721f
홍성준 <hackish@hanmail.net>
2019-06-16 17:25:02 +0900

    change project

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,68 @@
 <template>
-  <el-container>
-    <el-button>Default</el-button>
-    <section class="container">
-      <div>
-        <app-logo/>
-        <h1 class="title">
-          freevue-nuxt-todo
-        </h1>
-        <h2 class="subtitle">
-          Nuxt.js project
-        </h2>
-        <div class="links">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            class="button--green">Documentation</a>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            class="button--grey">GitHub</a>
-        </div>
+  <div class="container">
+    <div>
+      <logo />
+      <h1 class="title">
+        jj-coding-test
+      </h1>
+      <h2 class="subtitle">
+        My first-class Nuxt.js project
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green"
+        >Documentation</a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+        >GitHub</a>
       </div>
-    </section>
-  </el-container>
+    </div>
+  </div>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import Logo from '~/components/Logo.vue'
 
 export default {
   components: {
-    AppLogo
+    Logo
   }
 }
 </script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
--- plugins/element-ui.js
+++ plugins/element-ui.js
@@ -1,9 +1,5 @@
-import Vue from 'vue';
-import ElementUI from 'element-ui';
-import ko from 'element-ui/lib/locale/lang/ko';
+import Vue from 'vue'
+import Element from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/en'
 
-import 'element-ui/lib/theme-chalk/index.css'
-
-export default () => {
-  Vue.use(ElementUI, { ko });
-};
+Vue.use(Element, { locale })
