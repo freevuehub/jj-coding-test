01f7ab6fc97563c14e0c0bb25ed94ad68ada7a3d
홍성준 <hackish@hanmail.net>
2019-06-16 18:38:15 +0900

    [상태 대응] http 상태 error 대응

--- axios/Todo.js
+++ axios/Todo.js
@@ -1,11 +1,25 @@
 import Config from './Config';
+import { Notification } from 'element-ui';
+
+const ErrorNotification = msg => {
+  Notification.error({
+    title: 'Error',
+    message: msg,
+  });
+
+  return fn => fn();
+};
 
 export default {
   getList: () => {
     return new Promise((resolve, reject) => {
       Config.get(`/todo/list`)
         .then(({ data }) => resolve(data))
-        .catch((err) => reject(err));
+        .catch((err) => ErrorNotification(
+            err.response.data.error.message
+          )(
+            reject(err)
+        ));
     });
   },
 }
