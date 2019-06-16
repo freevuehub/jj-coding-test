1840e2189f6403e36e94c60761e49e27eec40502
홍성준 <hackish@hanmail.net>
2019-06-16 18:44:37 +0900

    [레이아웃] 완료 리스트 및 미완료 리스트 분기

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,21 @@
   <el-row :gutter="20">
     <el-col :span="12" :offset="6">
       <el-timeline v-for="l in __TodoList" :key="l.idx">
-        <el-timeline-item :timestamp="l.date" placement="top">
+        <el-timeline-item :timestamp="l.date" placement="top" color="#399f62">
           <el-card shadow="hover">
             <h4>{{ l.title }}</h4>
             <p>{{ l.comment }}</p>
+          </el-card>
+        </el-timeline-item>
+      </el-timeline>
+    </el-col>
+
+    <el-col :span="12" :offset="6">
+      <el-timeline>
+        <el-timeline-item timestamp="완료" placement="top">
+          <el-card shadow="never">
+            <h4>test</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam, deleniti earum exercitationem hic magnam nobis recusandae. A autem cumque ea, eum fugiat in ipsam ipsum minus molestias neque qui?</p>
           </el-card>
         </el-timeline-item>
       </el-timeline>
